8892895f3f0cf74e4c1a610c4ce30493a5cac709
Sunil Kumar <sunil.dhiman90@gmail.com>
2025-03-31 21:46:47 +0530

    use gradle-maven-publish-plugin

--- convention-plugins/build.gradle.kts
+++ convention-plugins/build.gradle.kts
@@ -3,5 +3,5 @@ plugins {
 }
 
 dependencies {
-    implementation(libs.nexus.publish)
+    implementation(libs.gradle.maven.publish.plugin)
 }
--- convention-plugins/src/main/kotlin/root.publication.gradle.kts
+++ convention-plugins/src/main/kotlin/root.publication.gradle.kts
@@ -1,21 +1,8 @@
 plugins {
-    id("io.github.gradle-nexus.publish-plugin")
+    id("com.vanniktech.maven.publish") apply false
 }
 
 allprojects {
     group = "io.github.sunildhiman90"
     version = "0.0.1-alpha-test"
-}
-
-nexusPublishing {
-    // Configure maven central repository
-    // https://github.com/gradle-nexus/publish-plugin#publishing-to-maven-central-via-sonatype-ossrh
-    repositories {
-        sonatype {  //only for users registered in Sonatype after 24 Feb 2021
-            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
-            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
-            username.set(System.getenv("ORG_GRADLE_PROJECT_mavenCentralUsername"))
-            password.set(System.getenv("ORG_GRADLE_PROJECT_mavenCentralPassword"))
-        }
-    }
 }

b7e200714914464e4131324b5fde3c083ce996b8
Sunil Kumar <sunil.dhiman90@gmail.com>
2025-04-01 12:16:59 +0530

    0.0.3-alpha03 version

--- convention-plugins/src/main/kotlin/root.publication.gradle.kts
+++ convention-plugins/src/main/kotlin/root.publication.gradle.kts
@@ -5,5 +5,5 @@ plugins {
 // these will be used for all projects ie. kmauth-core, kmauth-google etc.
 allprojects {
     group = "io.github.sunildhiman90"
-    version = "0.0.3-alpha02"
+    version = "0.0.3-alpha03"
 }

7a9cb615fa3d358e670baf617ef7718e5da62551
Sunil Kumar <sunil.dhiman90@gmail.com>
2025-04-01 10:59:31 +0530

    Test version: 0.0.2-alpha

--- convention-plugins/src/main/kotlin/root.publication.gradle.kts
+++ convention-plugins/src/main/kotlin/root.publication.gradle.kts
@@ -2,7 +2,8 @@ plugins {
     id("com.vanniktech.maven.publish") apply false
 }
 
+// these will be used for all projects ie. kmauth-core, kmauth-google etc.
 allprojects {
     group = "io.github.sunildhiman90"
-    version = "0.0.1-alpha-test"
+    version = "0.0.2-alpha"
 }

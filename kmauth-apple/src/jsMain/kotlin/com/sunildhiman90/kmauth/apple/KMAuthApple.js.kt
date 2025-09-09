4c7a98b6552999a65fb987f21faf9e807527093e
Sunil Kumar <sunil.dhiman90@gmail.com>
2025-09-09 13:58:14 +0530

    feat: Integrate deep link handling for Apple authentication across platforms

--- kmauth-apple/src/jsMain/kotlin/com/sunildhiman90/kmauth/apple/KMAuthApple.js.kt
+++ kmauth-apple/src/jsMain/kotlin/com/sunildhiman90/kmauth/apple/KMAuthApple.js.kt
@@ -1,7 +1,13 @@
 package com.sunildhiman90.kmauth.apple
 
+import com.sunildhiman90.kmauth.supabase.KMAuthSupabase
+import com.sunildhiman90.kmauth.supabase.deeplink.DeepLinkHandler
+
 
 actual object KMAuthApple {
     actual val appleAuthManager: AppleAuthManager
         get() = AppleAuthManagerJs()
+
+    actual fun deepLinkHandler(): DeepLinkHandler = KMAuthSupabase.deepLinkHandler()
+
 }
--- kmauth-apple/src/jvmMain/kotlin/com/sunildhiman90/kmauth/apple/KMAuthApple.jvm.kt
+++ kmauth-apple/src/jvmMain/kotlin/com/sunildhiman90/kmauth/apple/KMAuthApple.jvm.kt
@@ -1,6 +1,12 @@
 package com.sunildhiman90.kmauth.apple
 
+import com.sunildhiman90.kmauth.supabase.KMAuthSupabase
+import com.sunildhiman90.kmauth.supabase.deeplink.DeepLinkHandler
+
 actual object KMAuthApple {
     actual val appleAuthManager: AppleAuthManager
         get() = AppleAuthManagerJvm()
+
+    actual fun deepLinkHandler(): DeepLinkHandler = KMAuthSupabase.deepLinkHandler()
+
 }
